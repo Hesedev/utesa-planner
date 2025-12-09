33dd6d6a5eaf89fbf2038584bf074f87cea50e3f
Hesedev <hesedev@gmail.com>
2025-12-09 10:25:33 -0400

    feat: Header modified and project name changed to Pensum Planner

--- views/homeView.js
+++ views/homeView.js
@@ -3,18 +3,18 @@
 export function homeView() {
     return `
         <div class="text-center">
-            <h1 class="mb-3">Bienvenido a UTESA Planner</h1>
+            <h1 class="mb-3">¡Bienvenido a Pensum Planner!</h1>
             <p class="lead">
-                Organiza tus materias, crea planes de estudio y exporta tus pensums
-                de manera fácil y rápida.
+                Exporta/Importa tus pensums y crea planes de estudio 
+                optimizados de manera fácil y rápida.
             </p>
 
             <div class="mt-4">
-                <a href="#editor" class="btn btn-primary btn-lg me-2">
+                <a href="#editor" class="btn btn-warning btn-lg me-2">
                     Editor de Pensum
                 </a>
 
-                <a href="#planner" class="btn btn-outline-primary btn-lg">
+                <a href="#planner" class="btn btn-outline-success btn-lg">
                     Planificación
                 </a>
             </div>
